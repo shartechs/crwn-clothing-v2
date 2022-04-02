de5a1e250d7a496b4e721313a03c34fe461abcc9
Shartechs <shartechs@gmail.com>
2022-04-02 19:33:30 +0400

    added react-router and navigation component

---
+++ src/routes/Navigation/Navigation.jsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./Navigation.styles.scss";
+
+function Navigation() {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to={"/"}>
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation;
